4bc8a87b6b6f2a62e3c72affef43a7123765f23f
Kseniia <kseniiawest777@mail.ru>
2022-08-23 13:35:53 +0200

    feat: add task 2

--- JS_step_2/script.js
+++ JS_step_2/script.js
@@ -9,3 +9,42 @@ str.length - и получить её длину)
 "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 4) Потренироваться и переписать цикл еще двумя способами*/
 
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('yes');
+    } else {
+        console.log('no');
+        i--; //return on one cycle back
+    }
+};
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+}else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+;}
+
+
+console.log(personalMovieDB);
+
