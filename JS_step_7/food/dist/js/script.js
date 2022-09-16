35430c08cf3cb1d5d88393353f8b9cc12d68e1d1
Kseniia <kseniiawest777@mail.ru>
2022-09-16 15:17:22 +0200

    feat: create tabs and animation

--- JS_step_7/food/dist/js/script.js
+++ JS_step_7/food/dist/js/script.js
@@ -93,7 +93,44 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsContent = document.querySelectorAll('.tabcontent');
+  const tabsParent = document.querySelector('.tabheader__items');
 
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  } //default value 0 if nothing is passed to the function
+
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent(); //click event handler using delegation
+
+  tabsParent.addEventListener('click', event => {
+    const target = event.target; //for simplify
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
 
 /***/ })
 
