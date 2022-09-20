03af0c6e06bc0327cd3b5e265f46f718189a736a
Kseniia <kseniiawest777@mail.ru>
2022-09-20 17:08:11 +0200

    feat: create timer

--- JS_step_7/food/dist/js/script.js
+++ JS_step_7/food/dist/js/script.js
@@ -94,6 +94,7 @@
 /***/ (function(module, exports) {
 
 window.addEventListener('DOMContentLoaded', () => {
+  //TABS
   const tabs = document.querySelectorAll('.tabheader__item');
   const tabsContent = document.querySelectorAll('.tabcontent');
   const tabsParent = document.querySelector('.tabheader__items');
@@ -129,7 +130,69 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
-  });
+  }); //TIMER
+
+  const deadline = '2022-10-07'; //difference beetween curent time and deadline
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()); //calculate how many milliseconds in a day
+
+    const days = Math.floor(t / (1000 * 60 * 60 * 24)); //how many hours are left of the day
+
+    const hours = Math.floor(t / (1000 * 60 * 60) % 24); //how many minutes are left of the hour
+
+    const minutes = Math.floor(t / 1000 / 60 % 60); //how many seconds are left of the hour
+
+    const seconds = Math.floor(t / 1000 % 60); //return an object
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  ; //set clock
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000); //run function updateClock() every 1 second
+
+    updateClock(); //so that the timer does not blink
+    //update clock
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = getZero(t.days); // from return object in getTimeRemaining(endtime)
+
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds); //when deadline is the end
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+
+    ;
+  }
+
+  ;
+  setClock('.timer', deadline);
 });
 
 /***/ })
