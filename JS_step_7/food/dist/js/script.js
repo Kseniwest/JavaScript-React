bf6b54591ade54162be4c304880e7a7d07f8c392
Kseniia <kseniiawest777@mail.ru>
2022-11-25 16:56:44 +0100

    fix: start to do calculator

--- JS_step_7/food/dist/js/script.js
+++ JS_step_7/food/dist/js/script.js
@@ -3772,7 +3772,48 @@ window.addEventListener('DOMContentLoaded', () => {
         currentSlide.textContent = slideIndex;
       }
     });
-  });
+  }); //Calculator
+
+  const result = document.querySelector('.calculating__result span');
+  let sex, height, weight, age, ratio;
+
+  function calcTotal() {
+    if (!sex || !height || !weight || !age || !ratio) {
+      result.textContent = '0';
+      return;
+    }
+
+    if (sex === 'female') {
+      result.textContent = (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio;
+    } else {
+      result.textContent = (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio;
+    }
+  }
+
+  ;
+  calcTotal();
+
+  function getStaticInfo(parentSelector, activeClass) {
+    const elements = document.querySelectorAll(`${parentSelector} div`);
+    document.querySelector(parentSelector).addEventListener('click', e => {
+      if (e.target.getAttribute('data-ratio')) {
+        ratio = +e.target.getAttribute('data-ratio');
+      } else {
+        sex = e.target.getAttribute('id');
+      }
+
+      console.log(ratio, sex);
+      elements.forEach(elem => {
+        elem.classList.remove(activeClass);
+      });
+      e.target.classList.add(activeClass); //имеено тому диву на который кликнули назначаем класс
+    });
+  }
+
+  getStaticInfo('#gender', 'calculating__choose-item_active');
+  getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active'); // function getDynamicInfo (selectorInput){
+  //     const input = document.querySelector(selectorInput);
+  // };
 });
 
 /***/ })
